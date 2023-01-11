e6e172d39706e97595861d6026549ef84fa68084
Futureland3 <markmurai3@gmail.com>
2023-01-11 12:39:15 +0100

    Add class content's change through DOM, Delete div x and o, Change btw class's font size

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-$(document).ready(function(){
-    var height = $(window).height();
-    $('.grids').css('height', height);
-  });
+$('.grid-item').html("O");
+document.getElementsByClassName("grid-item")[0].innerHTML = 'X';
+document.getElementsByClassName("grid-item")[4].innerHTML = 'X'
+document.getElementsByClassName("grid-item")[8].innerHTML = 'X'
+
+// for(var i = 0; i < buttons.length; i++){
+//   buttons[i].classList.add('hidden');
+// }
+
